01142d1e2fe1f1e16331b97ae580c73051ffbc3d
Julrodriguez7 <jrodriguezf.922@gmail.com>
2023-12-05 15:55:44 -0300

    Navbar responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CartWidget } from '../CartWidget/CartWidget'
 import './NavBar.css'
 import { Link, NavLink } from 'react-router-dom'
 
 export const NavBar = () => {
+
+    const [isOpen, setIsOpen] = useState (false);
+
   return (
     <nav className='nav-bar'>
-        <Link className='link-title' to={`/`}>
+        <Link className='link-title' to={`/`} onClick={() => { setIsOpen(false);}}>
             <h1 className='title-nav'>ARROW</h1>
         </Link>
-        <div>
-            <NavLink to={`/category/pantalones`} className='btn-nav'>PANTALONES</NavLink>
-            <NavLink to={`/category/buzos`} className='btn-nav'>BUZOS</NavLink>
-            <NavLink to={`/category/remeras`} className='btn-nav'>REMERAS</NavLink>
-            <NavLink to={`/category/shorts`} className='btn-nav'>SHORTS</NavLink>
+        <div className={`nav-items ${isOpen && "open"}`}>
+            <NavLink to={`/category/pantalones`} className='btn-nav'  onClick={() => { setIsOpen(false);}}>PANTALONES</NavLink>
+            <NavLink to={`/category/buzos`} className='btn-nav'  onClick={() => { setIsOpen(false);}}>BUZOS</NavLink>
+            <NavLink to={`/category/remeras`} className='btn-nav'  onClick={() => { setIsOpen(false);}}>REMERAS</NavLink>
+            <NavLink to={`/category/shorts`} className='btn-nav'  onClick={() => { setIsOpen(false);}}>SHORTS</NavLink>
         </div>
         <div>
             <CartWidget />
+        </div>
+        <div className={`nav-toggle ${isOpen && "open"}`} onClick={ () => setIsOpen(!isOpen)}>
+            <span></span>
+            <span></span>
+            <span></span>
         </div>    
     </nav>
   )
